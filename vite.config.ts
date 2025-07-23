bf8989999186c287133f8ad0330416f08cd1c7ad
Ishashuklaa <146952923+Ishashuklaa@users.noreply.github.com>
2025-07-23 18:22:52 +0530

    Fix base path for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://culture-connect-platform-ae65.vercel.app',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
